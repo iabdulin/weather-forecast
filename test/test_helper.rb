1be3cb721e53b92fec0ffb5617dbcbd36e2663b6
Ildar Abdulin <ildar.abdulin@gmail.com>
2025-01-09 16:31:25 -0700

    Setting up test gems (VCR) and geocoder gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "minitest/mock"
 
 module ActiveSupport
   class TestCase
@@ -12,4 +13,19 @@ module ActiveSupport
 
     # Add more helper methods to be used by all tests here...
   end
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "test/vcr_cassettes"
+  config.hook_into :webmock
+
+  # Don't allow any real HTTP requests in tests
+  config.allow_http_connections_when_no_cassette = false
+
+  # # Optionally, configure cassette expiry for weather data
+  config.default_cassette_options = {
+    record: :once,            # Record first time, playback after
+    match_requests_on: [:method, :uri] # How to match requests
+    #   re_record_interval: 7.days  # Re-record after a week
+  }
 end
