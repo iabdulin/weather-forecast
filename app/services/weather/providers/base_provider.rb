017cdb16f93fedfe7c222857288ff46c72a52564
Ildar Abdulin <ildar.abdulin@gmail.com>
2025-01-11 11:04:35 -0700

    [docs] Added missing rdocs for Weather::Providers::BaseProvider

--- app/services/weather/providers/base_provider.rb
+++ app/services/weather/providers/base_provider.rb
@@ -1,8 +1,16 @@
 module Weather
   module Providers
+    # @abstract Base class for weather forecast providers
+    # Defines the interface and common functionality for all weather providers.
+    # Each provider must implement the {#get_forecast} method.
     class BaseProvider
+      # @return [ActiveSupport::Duration] Duration for which results should be cached
       CACHE_EXPIRY = 30.minutes
 
+      # Retrieves weather forecast for given coordinates
+      # @param coordinates [String] Location coordinates in "latitude,longitude" format
+      # @return [Hash] Weather forecast data
+      # @raise [NotImplementedError] if the subclass doesn't implement this method
       def get_forecast(coordinates)
         raise NotImplementedError, "Subclass #{self.class.name} must implement #get_forecast"
       end
